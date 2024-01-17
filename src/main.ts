eb2f484d7b30fa3f355e4300aecc8ed5ecf6a0ce
Gerard Paapu <gerard@gerardpaapu.com>
2024-01-18 10:39:13 +1300

    fallback to the title

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,40 @@ import type { Link } from './link';
 import toTimestamp from './to-timestamp';
 import Stories from './quizzes';
 
+const MONTH_NAMES = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
 async function* main(infra: Infra): AsyncGenerator<Link> {
   const recentQuizzes = await infra.fetchJson<Stories>(
     `https://www.stuff.co.nz/_json/national/quizzes`
   );
+  const now = infra.now();
 
   for (const {
     title,
     html_assets,
     datetime_display,
   } of recentQuizzes.stories) {
-    const timestamp = toTimestamp(infra.now());
+    const timestamp = toTimestamp(now);
+    const titleDate = `${
+      MONTH_NAMES[now.getMonth()]
+    } ${now.getDate()}, ${now.getFullYear()}`;
 
     if (
       title.indexOf('Sport') !== -1 ||
-      datetime_display.indexOf(timestamp) === -1
+      !(datetime_display.endsWith(timestamp) || title.endsWith(titleDate))
     ) {
       continue;
     }
